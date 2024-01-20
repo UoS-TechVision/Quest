f5c39cb1f66180cc85a2c0550fd4e872fef9ea1f
Imogen Pemberton <ipemberton1@sheffield.ac.uk>
2024-01-20 13:03:10 +0000

    implemented player facing direction depending on movement direction, player model updated to a stick man: no movement = 'standing' sprite, movement = 'running' sprite, added collision boxes to represent various objects which may be in the world. player cannot run through them, must go around them.

--- ProjectQuest/Assets/Joystick/JoystickMove.cs
+++ ProjectQuest/Assets/Joystick/JoystickMove.cs
@@ -4,46 +4,113 @@ using UnityEngine;
 
 public class JoystickMove : MonoBehaviour
 {
-    
+
     public Joystick movementJoystick;
     public float playerSpeed;
     private Rigidbody2D rb;
+    Vector2 move;
+    public bool isFacingRight = true;
+    float scaleX;
+    Vector3 currentScale;
+
+    public Sprite standing, running;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scaleX = (transform.localScale).x; // Calculates the initial x scale
+    }
+
+    private void Update() 
+    {
+        move.x = movementJoystick.Horizontal;
+        move.y = movementJoystick.Vertical;
+
+        float horizontalAxis = move.x;
+        float verticalAxis = move.y;
+        // zAxis calculates the direction the player is currently moving:
+        //          UP 00
+        // LEFT -90         90 RIGHT
+        //    DOWN -180  180 DOWN
+        float zAxis = Mathf.Atan2(horizontalAxis,verticalAxis) * Mathf.Rad2Deg;
+
+        // Player is moving to the left (as zAxis is negative)
+        if (zAxis < 0 && isFacingRight)
+        {
+            // If moving left and currently facing right, mirror the character
+            MirrorCharacter();
+        }
+        else if (zAxis > 0 && !isFacingRight)
+        {
+            // If moving right and currently not facing right, unmirror the character
+            UnmirrorCharacter();
+        }
+
+        // Update the character image based on the joystick input
+        if (movementJoystick.Direction.y == 0) 
+        {
+            // The player is not moving, so the sprite image is a standing man.
+            GetComponent<SpriteRenderer>().sprite = standing;
+        }
+        else 
+        {
+            // The player is moving, so the sprite image is a running man.
+            GetComponent<SpriteRenderer>().sprite = running;
+        }
     }
 
     private void FixedUpdate()
     {
-
-        if(transform.position.y >= 4.4375f)
+        // Implements the boundaries of the map. These values must be changed depending
+        // on the size of the character sprite. 
+        // The current values are for a pixel stick man. Commented values are for a white box.
+        if(transform.position.y >= 3.7f) // 4.4375f
         {
-            transform.position = new Vector3(transform.position.x, 4.4375f, 0.0f);
+            transform.position = new Vector3(transform.position.x, 3.7f, 0.0f);
         }
 
-        if(transform.position.y <= -4.4375f)
+        if(transform.position.y <= -3.7f) // -4.4375f
         {
-            transform.position = new Vector3(transform.position.x, -4.4375f, 0.0f);
+            transform.position = new Vector3(transform.position.x, -3.7f, 0.0f);
         }
 
-        if(transform.position.x >= 10.0445f)
+        if(transform.position.x >= 9f) // 10.0445f
         {
-            transform.position = new Vector3(10.0445f, transform.position.y, 0.0f);
+            transform.position = new Vector3(9f, transform.position.y, 0.0f);
         }
 
-        if(transform.position.x <= -10.0445f)
+        if(transform.position.x <= -9f) // -10.0445f
         {
-            transform.position = new Vector3(-10.0445f, transform.position.y, 0.0f);
+            transform.position = new Vector3(-9f, transform.position.y, 0.0f);
         }
 
+        // Calculates the velocity of the movement
         if(movementJoystick.Direction.y != 0)
         {
             rb.velocity = new Vector2(movementJoystick.Direction.x * playerSpeed, movementJoystick.Direction.y * playerSpeed);
-        } else
+        } 
+        else
         {
             rb.velocity = Vector2.zero;
         }
+    }
+
+    void MirrorCharacter()
+    {
+        // Mirror the character along the X-axis
+        currentScale = transform.localScale;
+        currentScale.x = -scaleX;
+        transform.localScale = currentScale;
+        isFacingRight = false;
+    }
+
+    void UnmirrorCharacter()
+    {
+        // Unmirror the character along the X-axis
+        currentScale = transform.localScale;
+        currentScale.x = scaleX;
+        transform.localScale = currentScale;
+        isFacingRight = true;
     }
 
 }
