0487f69505b2a3f15b15e485c98157371bec3fd3
TimOthry <timsonlee3@gmail.com>
2024-04-16 21:23:41 +0100

    Made tools draggable

--- ProjectQuest/Assets/Cat-puzzle/CatInteractions.cs
+++ ProjectQuest/Assets/Cat-puzzle/CatInteractions.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class CatInteractions : MonoBehaviour
 {
-   Vector3 mousePositionOffset;
-    public Material[] materials = new Material[2];
-    [SerializeField]public bool outlined = false;
+    Vector3 mousePositionOffset;
 
     private Vector3 GetMouseWorldPosition()
     {
@@ -16,44 +13,13 @@ public class CatInteractions : MonoBehaviour
 
     private void OnMouseDown()
     {   
-
         mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
-        outlined = !outlined;
-        if (outlined){
-            this.transform.parent.GetComponent<catChecker>().SelectLoop();
-            gameObject.GetComponent<SpriteRenderer>().material = materials[1];
-            outlined = true;
-
-        }
-        else{
-            gameObject.GetComponent<SpriteRenderer>().material = materials[0]; 
-        }
     }
 
     private void OnMouseDrag()
     {
-        if (outlined){
-            transform.position = GetMouseWorldPosition() + mousePositionOffset;
-        }
-         
+        transform.position = GetMouseWorldPosition() + mousePositionOffset;
     }
 
-    public void Unselect()
-    {
-        outlined = false;
-        gameObject.GetComponent<SpriteRenderer>().material = materials[0]; 
-    }
-
-    // Not needed - only dragging objects now
-    public void CheckWin()
-    {
-        if (this.tag == "Mittens"){
-            Debug.Log("Win");
-            SceneManager.LoadScene("SampleScene");
-        }
-        else{
-            Debug.Log("Lose");
-        }
-        Unselect();
-    }
+    // Optionally, you can include other methods like OnMouseUp() or OnCollisionEnter() if needed.
 }
